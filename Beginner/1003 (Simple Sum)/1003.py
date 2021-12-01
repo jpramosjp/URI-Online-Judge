7c9e5f94adc9ae6d2f5f0ad614594fa00ab143c5
jpramosjp <61604869+jpramosjp@users.noreply.github.com>
2021-12-01 13:18:03 -0300

    python

--- Beginner/1003 (Simple Sum)/1003.py
+++ Beginner/1003 (Simple Sum)/1003.py
@@ -1,26 +1,9 @@
-a= 0
+a= int(input())
+b= int(input())
 
-n = 10
+print("SOMA =", a+b)
 
 
-# input = cin
-# não existe variavel q linguagem é essa
-# nomeVariavel = int(2)
-#                float()
-#                bool() (True e False)
-
-# if a > b:
-#     print("que nazismo")
-# print("acabou o if ?")
-                #  i= i< i++
-# for i in range (3,30,7):
-#     break
-# else:
-#     print("for acabou")
-
-#   elif a<b:
-
-# while i < 2:
 
 
 
